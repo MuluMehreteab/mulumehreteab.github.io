a61b018f49283ea249e29317b9b434d26f5ed764
MuluMehreteab <45323018+MuluMehreteab@users.noreply.github.com>
2021-05-11 20:46:55 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById("main-heading").innerHTML = "New DOM Layout";
+document.getElementById("page-header").classList.replace("bg-dark" ,"bg-success")
+
+para1.innerHTML = 'The Document Object Model (DOM) is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure.';
+para2.innerHTML = 'The Document Object Model (DOM) is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects. That way, programming languages can connect to the page.';
+document.getElementById("btn").addEventListener("click", function() { para4.innerHTML ='The Document Object Model is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure wherein each node is an object representing a part of the document.'
+});
+
+
+document.getElementById('red').classList.replace('bg-white','bg-danger' )
+document.getElementById('blue').classList.replace('bg-white','bg-primary')
+document.getElementById('yellow').classList.replace('bg-white','bg-warning')
+document.getElementById('green').classList.replace('bg-white','bg-success')
+document.getElementById('black').classList.replace('bg-white', 'bg-dark')
+
+
+
